24e8fe80d9645366d79accf22f65e06fd6e4c3e5
Gonzalo <espindolamgonzalo@gmail.com>
2022-06-21 23:06:27 -0300

    fix routes

--- src/layout/LayoutApp.js
+++ src/layout/LayoutApp.js
@@ -17,15 +17,15 @@ function LayoutApp (props) {
             <main className="main">
                 <header className="header">
                     <p>{tokenDecode.sub}</p>
-                    <Link to='/login'><button onClick={closeSesion} className="secondary_button">Cerrar sesion</button></Link>
+                    <Link to='/gestor-de-gastos/login'><button onClick={closeSesion} className="secondary_button">Cerrar sesion</button></Link>
                 </header>
 
                 <nav className="nav">
                     <ul>
-                        <Link to='/home' className="link home"><i class='bx bxs-home' ></i> Inicio</Link>
-                        <Link to='/operations' className="link table"><i class='bx bxs-wallet-alt' ></i> Operaciones</Link>
-                        <Link to='/operations/incomes' className="link incomes"><i class='bx bxs-up-arrow-circle ' ></i> Ingresos</Link>
-                        <Link to='/operations/expenses' className="link expenses"><i class='bx bxs-down-arrow-circle '></i> Egresos</Link>
+                        <Link to='/gestor-de-gastos/home' className="link home"><i class='bx bxs-home' ></i> Inicio</Link>
+                        <Link to='/gestor-de-gastos/operations' className="link table"><i class='bx bxs-wallet-alt' ></i> Operaciones</Link>
+                        <Link to='/gestor-de-gastos/operations/incomes' className="link incomes"><i class='bx bxs-up-arrow-circle ' ></i> Ingresos</Link>
+                        <Link to='/gestor-de-gastos/operations/expenses' className="link expenses"><i class='bx bxs-down-arrow-circle '></i> Egresos</Link>
                     </ul>
                 </nav>
                 {props.children}
@@ -34,16 +34,16 @@ function LayoutApp (props) {
     }else {
         <main className="main">
                 <header className="header">
-                    <Link to='/login'><button className="secondary_button">Iniciar sesion</button></Link>
-                    <Link to='/register'><button className="secondary_button">Crear cuenta</button></Link>
+                    <Link to='/gestor-de-gastos/login'><button className="secondary_button">Iniciar sesion</button></Link>
+                    <Link to='/gestor-de-gastos/register'><button className="secondary_button">Crear cuenta</button></Link>
                 </header>
 
                 <nav className="nav">
                     <ul>
-                        <Link to='/home' className="link home"><i class='bx bxs-home' ></i> Inicio</Link>
-                        <Link to='/operations' className="link table"><i class='bx bxs-wallet-alt' ></i> Operaciones</Link>
-                        <Link to='/operations/incomes' className="link incomes"><i class='bx bxs-up-arrow-circle ' ></i> Ingresos</Link>
-                        <Link to='/operations/expenses' className="link expenses"><i class='bx bxs-down-arrow-circle '></i> Egresos</Link>
+                        <Link to='/gestor-de-gastos/home' className="link home"><i class='bx bxs-home' ></i> Inicio</Link>
+                        <Link to='/gestor-de-gastos/operations' className="link table"><i class='bx bxs-wallet-alt' ></i> Operaciones</Link>
+                        <Link to='/gestor-de-gastos/operations/incomes' className="link incomes"><i class='bx bxs-up-arrow-circle ' ></i> Ingresos</Link>
+                        <Link to='/gestor-de-gastos/operations/expenses' className="link expenses"><i class='bx bxs-down-arrow-circle '></i> Egresos</Link>
                     </ul>
                 </nav>
                 {props.children}
